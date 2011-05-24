d21c8737326c7c258ee8d9a5160f1a7ece4aba53
Marek Skrajnowski <m.skrajnowski@gmail.com>
2011-05-24 21:06:00 +0200

    Dodany kawalek kodu obrazujacy jak beda wygladac dane wejsciowe

--- R/test.R
+++ R/test.R
@@ -8,9 +8,9 @@ load.data = function(path) {
 	files.count = length(dir(path, recursive = TRUE))
 	
 	# use only words which appear in at least 2% of files
-	min.global.count = round(files.count*0.02)
+	min.global.count = round(files.count*0.01)
 	# skip files which appear in over 50% of files
-	max.global.count = round(files.count*0.5)
+	max.global.count = round(files.count*0.65)
 	
 	tmatrix = textmatrix(path, 
 			stemming = TRUE, 
@@ -25,9 +25,23 @@ load.data = function(path) {
 
 }
 
-data.path = "C:\\Users\\Marek.Marek-Netbook\\Documents\\Dane MOW\\processed"
-tmatrix = load.data("C:\\Users\\Marek.Marek-Netbook\\Documents\\Dane MOW\\easy_ham_2_out");
-write.table(tmatrix, "C:\\Users\\Marek.Marek-Netbook\\Documents\\Dane MOW\\easy_ham_2_out_tmatrix.txt")
+data.path = "F:\\Marek\\studia\\MOW\\Projekt\\Dane\\small set"
+# create document-term matrix
+tmatrix = load.data(data.path);
+# terms
+terms = dimnames(tmatrix)$terms
+# document names
+docs = dimnames(tmatrix)$docs
+
+# classes
+classes = rep(0, length(docs))
+classes[grep("spam", docs)] = 1
+names(classes) = docs
+
+write.table(tmatrix, file = paste(data.path, ".matrix.txt", sep=""))
+write.table(terms, file = paste(data.path, ".terms.txt", sep=""))
+write.table(docs, file = paste(data.path, ".docs.txt", sep=""))
+write.table(classes, file = paste(data.path, ".classes.txt", sep=""))
 
 
 
