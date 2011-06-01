7a457d69a7a17bd3e301074aa18725304f41e428
Michal <kolodziejski.m@gmail.com>
2011-06-01 04:12:32 +0200

    Opis uzycia + poprawka w klasyfikacji bayesowskiej z uzyciem klaR (w sumie niepotrzebna, podczas predict wylatuje blad ktorego nie ma przy e1071)

--- R/classify-naive-bayes.R
+++ R/classify-naive-bayes.R
@@ -2,20 +2,13 @@ library("klaR")
 
 classify.naiveBayes = function(trainingSet, classes){
     # przeksztalcenie na macierz binarna
-    #for(i in 1:dim(trainingSet)[2]){
-    #    trainingSet[trainingSet[,i]>0, i] = 1
-    #}
     trainingSet[trainingSet[,] > 0] = 1
 
     # polaczenie transponowanej macierzy slow z klasami i stworzenie data.frame
-    tsData <- data.frame(cbind(t(trainingSet), classes));
+    tsData <- data.frame(cbind(t(trainingSet)));
     
     # utworzenie klasyfikatora, usekernel uzyte dla przypadkow gdy wariancja klas jest zerowa w ramach rozpatrywanego atrybutu
     nb <- NaiveBayes(tsData, factor(tsData$classes), usekernel = TRUE);
     return(nb);
 }
-
-
-#nb <- classify.naiveBayes(tmatrix, classes);
-#predict(nb, t(tmatrix));
 
