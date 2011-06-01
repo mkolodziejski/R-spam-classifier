8fbbc4307cad750df10e55ff1ff514c49243639a
Marek Skrajnowski <m.skrajnowski@gmail.com>
2011-06-01 22:41:58 +0200

    Juz dzialajacy klasyfikator knn, ale poki co sprawdzany tylko na zbiorze danych iris.

--- R/classify-knn.R
+++ R/classify-knn.R
@@ -34,32 +34,33 @@ euclidean.dist = function(x,y) {
 	return(t(d))
 }
 
-classify.knn = function(x, ...) UseMethod("classify.knn", ...)
+classify.knn = function(x, ...) UseMethod("classify.knn")
 	
 classify.knn.default = function(training.set, classes, 
-												k=3, dist.function="cosine", minkowski.power=2, 
+												k=3, dist.function=cosine.dist, minkowski.power=2, 
 												agg.method = "majority", ties.method = "first", ...) 
 {
 	result = list( training.set = training.set, classes = classes )
 				
 	result$parameters = list(	k=k, dist.function = dist.function, minkowski.power = minkowski.power, 
-										agg.method = agg.method, ties.method = ties.method )
+										agg.method = agg.method, ties.method = ties.method, formula = NULL )
 	class(result) = "knn"
 	attr(result,'call') <- match.call()
 	return(result)
 }
 
 classify.knn.formula = function(formula, data, 
-												k=3, dist.function=, minkowski.power=2, 
-												agg.method = if (is.factor(y)) "majority" else "mean", ties.method = "min", ...) 
+												k=3, dist.function=cosine.dist, minkowski.power=2, 
+												agg.method = "majority", ties.method = "first", ...) 
 {
 	# parse the formula
 	mf = model.frame(formula=formula, data=data)
-	trainingSet = model.matrix(attr(mf, "terms"), data=mf)
+	trainingSet = model.matrix(attr(mf, "terms"), data=mf)[,-1]
 	classes = model.response(mf)
 	
 	# pass to the default function
 	result <- classify.knn.default(trainingSet, classes, k, dist.function, minkowski.power, agg.method, ties.method, ...)
+	result$parameters$formula = formula
 	attr(result,'call') <- match.call()
 	attr(result,'formula') <- formula
 	return(result)
@@ -67,25 +68,72 @@ classify.knn.formula = function(formula, data,
 }
 
 predict.knn = function(object, newdata = NULL, ...) {
+	
 	params = object$parameters
 	train = object$training.set
-	test = newdata
+	
+	if(is.null(params$formula)) {
+		test = newdata
+	}
+	else {
+		test = model.matrix(params$formula, newdata)[,-1]
+	}
+	
+	test.samples = dim(test)[1]
 	classes = object$classes
 	
-	# cosine distance not supported by knn.dist
-	distance = params$distance.function(object$training.set, newdata)
+	distance = params$dist.function(object$training.set, test)
 	
-	ranks = t(apply(distance, 1,    function(x) rank(x, ties.method = ties.meth)))
-	new.classes = apply(ranks, 1, function(x) apply(data.frame(classes[x <= k]),	2, agg.meth))
+	ranks = apply(distance, 1,    function(x) rank(x, ties.method = params$ties.method))
+	matches = apply(ranks, 2, function(x) { which(x<=params$k) })
+	matching.classes = array(classes[matches], dim(matches))
+	
+	classes = aggregate(matching.classes, list(rep(TRUE,params$k)), params$agg.method)[,-1]
+	
+	i = cbind(rep(1:test.samples, each=params$k), c(matches))
+	matches.dist = array(distance[i], dim(matches))
+	
+	res = data.frame(Classes = t(classes), Matches = t(matches), 
+								Matching.Classes = t(matching.classes), Match.Distance = t(matches.dist))
+						
+	dimnames(res)[1] = dimnames(test)[1]
 		
-	return(new.classes)
+	class(res) = c("predict.knn", class(res))
+	return(res)
 			
 }
 
 
-(r1 <- rank(x1 <- c(3, 1, 4, 15, 92)))
-x2 <- c(3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5)
-names(x2) <- letters[1:11]
-(r2 <- rank(x2)) # ties are averaged
+test.predict = function(test.size = 0.2, k=5) {
+	data(iris)
+	shuffle = sample(dim(iris)[1])
+	s.iris = iris[shuffle,]
+	split = (1-test.size)*length(shuffle)
+	train = s.iris[1:(split-1),]
+	test = s.iris[split:length(shuffle),]
+	classes = test$Species
+	test$Species = factor("?")
+	
+	knn.obj = classify.knn(Species ~ ., data = train, k=k, dist.function = euclidean.dist)
+	p = predict(knn.obj, newdata = test)
+	
+	test$Species = classes
+	
+	#print(p)
+	cat("\n--------------------------------------\n\n")
+	error.indices = which(test$Species != p$Classes)
+	
+	print(paste("Errors:", length(error.indices)))
+	print(paste("Effectiveness:", round(1-length(error.indices)/dim(test)[1], digits = 2) ))
+	
+	if(length(error.indices) > 0) {
+		print("Bad Samples:")
+		report = cbind(test$Species[error.indices],p[error.indices,])
+		dimnames(report)[2][[1]][1] = "Real Class"
+		dimnames(report)[2][[1]][2] = "Predicted Class"
+		print(t(report))
+	}
+	
+}
 
 
