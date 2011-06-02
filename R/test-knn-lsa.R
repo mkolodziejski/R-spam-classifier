926cff6d9e9435c146bb84de310428dd128878e8
Marek Skrajnowski <m.skrajnowski@gmail.com>
2011-06-02 21:07:54 +0200

    Dodany test dla knn+LSA

---
+++ R/test-knn-lsa.R
@@ -0,0 +1,35 @@
+# 
+# Author: Michal Kolodziejski
+###############################################################################
+
+source("classify-knn-lsa.R")
+
+test.knn = function(dataPath, k=3, samplesNum = 1000, testSetPart = 0.05, dist.function = cosine.dist){
+    ts <- load.mail(c(dataPath), samples=samplesNum, shuffle.samples=TRUE, min.glob.word.freq = 0.01)
+	load.summary <- summary(ts)
+	
+    tsM <- ts[, -1]
+    tsM[tsM[,]>0]=1
+    tsClass <- factor(ts[, 1])
+
+    realSamplesNum <- dim(ts)[1];
+    testSetCount <- realSamplesNum * testSetPart;
+    trainingSetCount <- realSamplesNum - testSetCount;
+    testSetIndex <- trainingSetCount + 1;
+    
+    knn.classifier <- classify.knn.lsa(tsM[1:trainingSetCount,], tsClass[1:trainingSetCount], k=k)
+    knn.pred = predict(knn.classifier, newdata = tsM[testSetIndex:realSamplesNum,])
+	
+    goodResults <- 0;    
+    for(predResult in (knn.pred$Classes[] == tsClass[testSetIndex:realSamplesNum])){
+        if(predResult == TRUE)
+            goodResults <- goodResults + 1;
+    }
+
+    result = list()
+    class(result) = "test.knn"
+	result$load.summary <- load.summary
+    result$effectiveness <- goodResults / testSetCount;
+    return(result);
+}
+
