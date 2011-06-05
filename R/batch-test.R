3f0e96fdcc3e5d7ba547c02206db65380575d1e1
Marek Skrajnowski <m.skrajnowski@gmail.com>
2011-06-05 16:00:00 +0200

    Kolejne testy roznych wariantow algorytmow knn.

--- R/batch-test.R
+++ R/batch-test.R
@@ -11,6 +11,15 @@ source("classify-knn-lsa.R")
 
 bayes.classes = function(res) res;
 knn.classes = function(res) res$Classes;
+knn.tresholded.classes = function(tresh = 0.51) {
+	function(res) {
+		classes = res$Classes
+		majority = res$Majority
+		classes[classes == "spam" & majority < tresh] = "ham"
+		classes[classes == "ham"  & (1-majority) >= tresh] = "spam"
+		return(classes)
+	}
+}
 
 batch = list(
 		
@@ -19,16 +28,36 @@ batch = list(
 			#list(name = "knn.k=3", 		classify = "classify.knn", get.classes = knn.classes, args = list(k=3) ),
 			#list(name = "knn.k=7", 		classify = "classify.knn", get.classes = knn.classes, args = list(k=7) ),
 			
-			list(name = "knn.lsa.k=3.lsa.share=0.5", 	classify = "classify.knn.lsa", get.classes = knn.classes, args = list(k=3, lsa.dim = dimcalc_share(share=0.5)) ),
-			list(name = "knn.lsa.k=7.lsa.share=0.5", 	classify = "classify.knn.lsa", get.classes = knn.classes, args = list(k=7, lsa.dim = dimcalc_share(share=0.5)) ),
-			list(name = "knn.lsa.k=3.lsa.share=0.7", 	classify = "classify.knn.lsa", get.classes = knn.classes, args = list(k=3, lsa.dim = dimcalc_share(share=0.7)) ),
-			list(name = "knn.lsa.k=7.lsa.share=0.7", 	classify = "classify.knn.lsa", get.classes = knn.classes, args = list(k=7, lsa.dim = dimcalc_share(share=0.7)) ),
-			list(name = "knn.lsa.k=3.lsa.share=0.9", 	classify = "classify.knn.lsa", get.classes = knn.classes, args = list(k=3, lsa.dim = dimcalc_share(share=0.9)) ),
-			list(name = "knn.lsa.k=7.lsa.share=0.9", 	classify = "classify.knn.lsa", get.classes = knn.classes, args = list(k=7, lsa.dim = dimcalc_share(share=0.9)) )
+			#list(name = "knn.euclidean.lsa.k=3.lsa.share=0.1", 	classify = "classify.knn.lsa", get.classes = knn.classes, args = list(k=3, lsa.dim = dimcalc_share(share=0.1), dist.function = euclidean.dist) ),
+			#list(name = "knn.euclidean.lsa.k=7.lsa.share=0.1", 	classify = "classify.knn.lsa", get.classes = knn.classes, args = list(k=7, lsa.dim = dimcalc_share(share=0.1), dist.function = euclidean.dist) ),
+			#list(name = "knn.euclidean.lsa.k=3.lsa.share=0.3", 	classify = "classify.knn.lsa", get.classes = knn.classes, args = list(k=3, lsa.dim = dimcalc_share(share=0.3), dist.function = euclidean.dist) ),
+			#list(name = "knn.euclidean.lsa.k=7.lsa.share=0.3", 	classify = "classify.knn.lsa", get.classes = knn.classes, args = list(k=7, lsa.dim = dimcalc_share(share=0.3), dist.function = euclidean.dist) ),
+			#list(name = "knn.euclidean.lsa.k=3.lsa.share=0.5", 	classify = "classify.knn.lsa", get.classes = knn.classes, args = list(k=3, lsa.dim = dimcalc_share(share=0.5), dist.function = euclidean.dist) ),
+			#list(name = "knn.euclidean.lsa.k=7.lsa.share=0.5", 	classify = "classify.knn.lsa", get.classes = knn.classes, args = list(k=7, lsa.dim = dimcalc_share(share=0.5), dist.function = euclidean.dist) ),
+			#list(name = "knn.euclidean.lsa.k=3.lsa.share=0.7", 	classify = "classify.knn.lsa", get.classes = knn.classes, args = list(k=3, lsa.dim = dimcalc_share(share=0.7), dist.function = euclidean.dist) ),
+			#list(name = "knn.euclidean.lsa.k=7.lsa.share=0.7", 	classify = "classify.knn.lsa", get.classes = knn.classes, args = list(k=7, lsa.dim = dimcalc_share(share=0.7), dist.function = euclidean.dist) ),
+			
+			#list(name = "knn.cosine.lsa.k=3.lsa.share=0.1", 	classify = "classify.knn.lsa", get.classes = knn.classes, args = list(k=3, lsa.dim = dimcalc_share(share=0.1), dist.function = cosine.dist) ),
+			#list(name = "knn.cosine.lsa.k=7.lsa.share=0.1", 	classify = "classify.knn.lsa", get.classes = knn.classes, args = list(k=7, lsa.dim = dimcalc_share(share=0.1), dist.function = cosine.dist) ),
+			#list(name = "knn.cosine.lsa.k=3.lsa.share=0.3", 	classify = "classify.knn.lsa", get.classes = knn.classes, args = list(k=3, lsa.dim = dimcalc_share(share=0.3), dist.function = cosine.dist) ),
+			#list(name = "knn.cosine.lsa.k=7.lsa.share=0.3", 	classify = "classify.knn.lsa", get.classes = knn.classes, args = list(k=7, lsa.dim = dimcalc_share(share=0.3), dist.function = cosine.dist) ),
+			#list(name = "knn.cosine.lsa.k=3.lsa.share=0.5", 	classify = "classify.knn.lsa", get.classes = knn.classes, args = list(k=3, lsa.dim = dimcalc_share(share=0.5), dist.function = cosine.dist) ),
+			#list(name = "knn.cosine.lsa.k=7.lsa.share=0.5", 	classify = "classify.knn.lsa", get.classes = knn.classes, args = list(k=7, lsa.dim = dimcalc_share(share=0.5), dist.function = cosine.dist) ),
+			#list(name = "knn.cosine.lsa.k=3.lsa.share=0.7", 	classify = "classify.knn.lsa", get.classes = knn.classes, args = list(k=3, lsa.dim = dimcalc_share(share=0.7), dist.function = cosine.dist) ),
+			#list(name = "knn.cosine.lsa.k=7.lsa.share=0.7", 	classify = "classify.knn.lsa", get.classes = knn.classes, args = list(k=7, lsa.dim = dimcalc_share(share=0.7), dist.function = cosine.dist) )
+		
+			list(name = "knn.cosine.lsa.k=11.lsa.share=0.3.majority=4/11", classify = "classify.knn.lsa", get.classes = knn.tresholded.classes(4/11), args = list(k=11, lsa.dim = dimcalc_share(share=0.3), dist.function = cosine.dist) ),
+			list(name = "knn.cosine.lsa.k=11.lsa.share=0.3.majority=5/11", classify = "classify.knn.lsa", get.classes = knn.tresholded.classes(5/11), args = list(k=11, lsa.dim = dimcalc_share(share=0.3), dist.function = cosine.dist) ),
+			list(name = "knn.cosine.lsa.k=11.lsa.share=0.3.majority=6/11", classify = "classify.knn.lsa", get.classes = knn.tresholded.classes(6/11), args = list(k=11, lsa.dim = dimcalc_share(share=0.3), dist.function = cosine.dist) ),
+			list(name = "knn.cosine.lsa.k=11.lsa.share=0.3.majority=7/11", classify = "classify.knn.lsa", get.classes = knn.tresholded.classes(7/11), args = list(k=11, lsa.dim = dimcalc_share(share=0.3), dist.function = cosine.dist) ),
+			list(name = "knn.cosine.lsa.k=11.lsa.share=0.3.majority=8/11", classify = "classify.knn.lsa", get.classes = knn.tresholded.classes(8/11), args = list(k=11, lsa.dim = dimcalc_share(share=0.3), dist.function = cosine.dist) ),
+			list(name = "knn.cosine.lsa.k=11.lsa.share=0.3.majority=9/11", classify = "classify.knn.lsa", get.classes = knn.tresholded.classes(9/11), args = list(k=11, lsa.dim = dimcalc_share(share=0.3), dist.function = cosine.dist) ),
+			list(name = "knn.cosine.lsa.k=11.lsa.share=0.3.majority=10/11",classify = "classify.knn.lsa", get.classes = knn.tresholded.classes(10/11), args = list(k=11, lsa.dim = dimcalc_share(share=0.3), dist.function = cosine.dist) ),
+			list(name = "knn.cosine.lsa.k=11.lsa.share=0.3.majority=11/11",classify = "classify.knn.lsa", get.classes = knn.tresholded.classes(11/11), args = list(k=11, lsa.dim = dimcalc_share(share=0.3), dist.function = cosine.dist) )
 			
 		)
 
-cat(paste(date(), "Started batch test\n\n")); 
+cat(paste("\n", date(), "Started batch test\n\n")); 
 data = load.mail(data.path, samples, shuffle.samples = TRUE, min.glob.word.freq = 0.01, max.words = max.words)
 cat(paste(date(), "Data loaded, summary:\n\n"));
 print(summary(data))
@@ -46,6 +75,10 @@ data.sets = cbind(round(begin), round(end))
 errors = matrix(0,length(batch),cv.sets)
 dimnames(errors)[[1]] = lapply(batch, function(x) x$name)
 
+true.negatives = errors
+true.positives = errors
+false.negatives = errors
+false.positives = errors
 model.time = errors
 pred.time = errors
 
@@ -53,7 +86,7 @@ cpu.time = function() sum(proc.time()[c(1,2)])
 
 for(si in 1:cv.sets) {
 	
-	cat(paste(date(), "Started set", si, "\n\n"));
+	cat(paste("\n",date(), "Started set", si, "\n\n"));
 	
 	test.indices = data.sets[si,1]:data.sets[si,2]
 	test = data[test.indices,]
@@ -80,26 +113,38 @@ for(si in 1:cv.sets) {
 		pred.time[bi,si] = cpu.time() - current.time
 				
 		pred.classes = factor(params$get.classes(pred), levels = c("spam","ham"))
-		errors[bi,si] = sum(pred.classes!=test[,1])
+		true.positives[bi,si]  = sum(pred.classes==test[,1] & pred.classes == 'spam')
+		true.negatives[bi,si]  = sum(pred.classes==test[,1] & pred.classes == 'ham')
+		false.positives[bi,si] = sum(pred.classes!=test[,1] & pred.classes == 'spam')
+		false.negatives[bi,si] = sum(pred.classes!=test[,1] & pred.classes == 'ham')
 		
-		cat(paste(date(), " Finished ", params$name," for set", si,", errors so far:\n", sep = ""));
-		print(errors)
+		progress = ((si-1)*length(batch)+bi-1)/cv.sets/length(batch)
+		cat(paste(date(), " Finished ", params$name," for set ", si," (",round(100*progress),"%)\n", sep = ""));
 		
 	}
 	
 }
 
-cat(paste(date(), "\nFinished batch test\n\n"));
+cat(paste("\n", date(), "Finished batch test\n\n"));
+
+errors = false.negatives + false.positives
+tp.rate = rowSums(true.positives)  / rowSums(true.positives+false.negatives)
+fp.rate = rowSums(false.positives) / rowSums(false.positives+true.negatives)
 
 performance = 1-rowSums(errors)/sample.count
 total.model.time = rowSums(model.time)
 total.pred.time = rowSums(pred.time)
 total.time = total.model.time + total.pred.time
 
-cat("Errors per set:\n")
+cat("\nErrors per set:\n")
 print(errors)
+
 cat("\nPerformance:\n")
 print(cbind(performance))
+cat("\nTP Rate:\n")
+print(cbind(tp.rate))
+cat("\nFP Rate:\n")
+print(cbind(fp.rate))
 cat("\nCPU Time:\n")
 print(cbind(total.time))
 
