c1f654fc07075a0287dbdda803675e5dd24e746d
Michal <kolodziejski.m@gmail.com>
2011-06-07 01:08:09 +0200

    Okomentowanie kodu - dopisanie i przepisanie na jezyk angielski, oczyszczenie ze zbednych linii

--- R/classify-naive-bayes.R
+++ R/classify-naive-bayes.R
@@ -1,13 +1,27 @@
+# 
+# Author: Michal Kolodziejski
+###############################################################################
+
 library("klaR")
 
+# DEPRECATED!!! "test.naiveBayes.e1071" should be used instead.
+#
+# Transforms given training-set matrix into binary matrix and performs classification
+# using NaiveBayes function from klaR library.
+# Arguments:
+#       trainingSet - matrix with training-set data
+#       classes - vector with classes corresponding to examples from training-set in the same order.
+#
+# Returns naive bayes classifier.
+#
 classify.naiveBayes = function(trainingSet, classes){
-    # przeksztalcenie na macierz binarna
+    # transformation into binary matrix
     trainingSet[trainingSet[,] > 0] = 1
 
-    # polaczenie transponowanej macierzy slow z klasami i stworzenie data.frame
+    # creates data.frame with data from training-set
     tsData <- data.frame(cbind(t(trainingSet)));
     
-    # utworzenie klasyfikatora, usekernel uzyte dla przypadkow gdy wariancja klas jest zerowa w ramach rozpatrywanego atrybutu
+    # creates naive bayes classifier, usekernel used in case where class variance is equal to zero for attribute
     nb <- NaiveBayes(tsData, factor(tsData$classes), usekernel = TRUE);
     return(nb);
 }
--- R/test-naive-bayes-e1071.R
+++ R/test-naive-bayes-e1071.R
@@ -5,19 +5,35 @@
 library("e1071")
 source("load-mail.R")
 
+
+# Creates naive bayes classifier and performs tests on it.
+# Arguments:
+#        dataPath - path to the directory containing messages to be read
+#        samplesNum - total number of messages to be read
+#        spam.part - fraction of all messages that should be spam
+#        testSetPart - fraction of all messages that should form test-set data
+#
+# Returns effectiveness of classifier, summary of loaded messages and classification statistics:
+# number of false-positive errors, false-negative errors, true-positive and true-negative classifications,
+# true-positive rate and false-positive rate.
+#
 test.naiveBayes.e1071 = function(dataPath, samplesNum = 1000, spam.part = 0.5, testSetPart = 0.05){
+    # read data
     ts <- load.mail(c(dataPath), samples=samplesNum, shuffle.samples=TRUE, min.glob.word.freq = 0.01, spam.probability = spam.part)
 	load.summary <- summary(ts)
 	
+    # transform into binary data
     tsM <- ts[, -1]
     tsM[tsM[,]>0]=1
     tsClass <- factor(ts[, 1])
 
+    # distribute messages between training and test set
     realSamplesNum <- dim(ts)[1];
     testSetCount <- realSamplesNum * testSetPart;
     trainingSetCount <- realSamplesNum - testSetCount;
     testSetIndex <- trainingSetCount + 1;
     
+    # create classifier and test it
     enb <- naiveBayes(tsM[1:trainingSetCount,], tsClass[1:trainingSetCount]);
     pred <- predict(enb, tsM[testSetIndex:realSamplesNum,])
 
@@ -41,21 +57,8 @@ test.naiveBayes.e1071 = function(dataPath, samplesNum = 1000, spam.part = 0.5, t
     }
 
 
-    #for(predResult in (pred[] == tsClass[testSetIndex:realSamplesNum])){
-    #    if(predResult == TRUE)
-    #        goodResults <- goodResults + 1;
-    #}
-
-
-
     result = list()
     class(result) = "test.naiveBayes.e1071"
-    #result$trainingSet <- tsMD[1:trainingSetCount,];
-    #result$treiningSetClasses <- tsClass[1:trainingSetCount];
-    #result$testSet <- tsMD[testSetIndex:samplesNum,];
-    #result$testSetClasses <- tsClass[testSetIndex:samplesNum];
-    #result$classifier <- enb;
-    #result$prediction <- pred;
 	result$load.summary <- load.summary
     result$effectiveness <- goodResults / testSetCount;
     result$TP <- tp;
